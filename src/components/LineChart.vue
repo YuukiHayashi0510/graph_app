a62df8b938ced6d82ac7fb2a3b477edcadccf674
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2023-02-01 23:35:12 +0900

    feat: chartjs

---
+++ src/components/LineChart.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <Line v-if="loaded" :data="chartData" />
+  </div>
+</template>
+
+<script>
+import { Line } from "vue-chartjs"
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+} from "chart.js"
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement
+)
+
+export default {
+  name: "LineChart",
+  components: { Line },
+  props: {
+    chartData: {
+      type: Object,
+    },
+    loaded: {
+      type: Boolean,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.container {
+  width: 60%;
+  margin: 1rem auto;
+}
+</style>
